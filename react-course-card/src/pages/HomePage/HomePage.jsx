0ffac99aa263b39e838fe9cb193e7b8a44672c9d
Richard Yee <richardyee@192-168-1-6.tpgi.com.au>
2024-11-10 20:00:55 +1000

    add buttom to home page

--- react-course-card/src/pages/HomePage/HomePage.jsx
+++ react-course-card/src/pages/HomePage/HomePage.jsx
@@ -5,6 +5,11 @@ export default function HomePage() {
   return (
     <section className="homepage">
       <h1>Welcome to Course Card</h1>
+      <div className="login">
+        <input type="text" placeholder="Account Name (abc)" />
+        <input type="password" placeholder="Password (123)" />
+        <button>Sign In</button>
+      </div>
     </section>
   );
 }
