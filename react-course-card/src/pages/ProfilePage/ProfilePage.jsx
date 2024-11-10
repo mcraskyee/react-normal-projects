ea18960a7912d2fcd59cab60efaa1f851079dcd1
Richard Yee <richardyee@192-168-1-6.tpgi.com.au>
2024-11-10 18:43:15 +1000

    change css styles

--- react-course-card/src/pages/ProfilePage/ProfilePage.jsx
+++ react-course-card/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "./ProfilePage.css";
 
 export default function ProfilePage() {
   return (
-    <div>
+    <div className="profile-page">
       <h1>Profile Page</h1>
       <p>Displayed when you logged in.</p>
     </div>
